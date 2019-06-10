d4ec98757a16d306731b39648cf0fac34adace15
Bryant Pong <rcxking@gmail.com>
2019-06-09 22:05:43 -0700

    Issue #1: Create Initial main.cpp and CMakeLists.txt
    This commit creates an empty main.cpp and the required CMakeLists.txt
    to build the program.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ * Runner of an Extended Kalman Filter for a 2-Dimensional
+ * planar robot.
+ *
+ * Bryant Pong
+ * 6/9/19
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
